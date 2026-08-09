0da96b9ba9d621b5ee4e9c11468ac12b7ab72a70
princesingh0f5-byte <princesingh0f5@gmail.com>
2026-08-09 17:21:43 +0530

    Square on number till n

---
+++ 03_problem.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a Number: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << " " << "square = " << i * i << "\n";
+    }
+    return 0;
+}
