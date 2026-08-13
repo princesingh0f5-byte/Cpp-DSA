6228414cd7740f282692b9e4d2c46ae7912d07b0
princesingh0f5-byte <princesingh0f5@gmail.com>
2026-08-13 20:10:48 +0530

    Prime or not

---
+++ 09_problem.cpp
@@ -0,0 +1,29 @@
+// prime or not
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter number : ";
+    cin >> n;
+
+    if (n < 2)
+    {
+        cout << "Not prime";
+        return 0;
+    }
+
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << "Not prime";
+            return 0;
+        }
+    }
+
+    cout << "Prime";
+
+    return 0;
+}
